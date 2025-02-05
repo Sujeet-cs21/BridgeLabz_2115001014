9174e92c148409c6f2057f102a13286d37991099
Sujeet-cs21 <sucsjeet@gmail.com>
2025-02-05 23:14:55 +0530

    [Sujeet] Add : solved practice problem of Object Modeling

---
+++ PP_05-02-25/PP_05-02-2025/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PP_05_02_2025;
+
+namespace PP_05_02_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Main1();
+            //Class2.Main2();
+            //Class3.Main3();
+        }
+    }
+}
